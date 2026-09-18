9d2c931f2b5fa3976b32542904eac1a42b37d007
Bui Van-Phuc <bvphuc28@gmail.com>
2026-09-18 14:43:30 +0200

    Add shared real-photo visuals for site hubs

---
+++ src/data/hubVisuals.js
@@ -0,0 +1,90 @@
+export const hubVisuals = {
+  planningHero: {
+    src: 'https://images.unsplash.com/photo-1528127269322-539801943592?auto=format&fit=crop&w=1800&q=84',
+    alt: 'Grønt vietnamesisk landskab',
+    caption: 'Start med rute, rejsetid og antal stop. De valg gør resten af bookingen markant lettere.',
+    ratio: '16 / 7',
+  },
+  itinerariesHero: {
+    src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Vietnam%20landscape.jpg?width=1800',
+    alt: 'Landskab i Vietnam',
+    caption: 'Vietnam er langt fra nord til syd. En god rejseplan handler derfor lige så meget om transport som om seværdigheder.',
+    ratio: '16 / 7',
+  },
+  toursHero: {
+    src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Ha%20long%20bay%20cruise%20%E3%83%8F%E3%83%AD%E3%83%B3%E6%B9%BE%E3%82%AF%E3%83%AB%E3%83%BC%E3%82%BA%20-%2053164482453.jpg?width=1800',
+    alt: 'Cruisebåd i Ha Long Bay',
+    caption: 'Organiserede ture giver mest mening, når de løser transport, båd eller adgang - ikke bare fordi noget er populært.',
+    ratio: '16 / 7',
+  },
+  hotelsHero: {
+    src: 'https://images.unsplash.com/photo-1559592413-7cec4d0cae2b?auto=format&fit=crop&w=1800&q=84',
+    alt: 'Gadeliv og hoteller i Hoi An',
+    caption: 'Det rigtige område betyder ofte mere end én ekstra hotelstjerne.',
+    ratio: '16 / 7',
+  },
+  hanoiThreeDaysHero: {
+    src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Hoan%20Kiem%20Lake%20photo.jpg?width=1800',
+    alt: 'Hoan Kiem-søen i Hanoi',
+    caption: 'Tre dage i Hanoi fungerer bedst, når hver dag får sit eget område og der stadig er plads til mad, kaffe og små omveje.',
+    ratio: '16 / 7',
+  },
+  hanoiThreeDaysHistory: {
+    src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Temple%20of%20Literature%20Hanoi%201.jpg?width=1400',
+    alt: 'Litteraturtemplet i Hanoi',
+    caption: 'Litteraturtemplet passer naturligt ind på historiens dag vest for centrum.',
+    ratio: '16 / 9',
+  },
+  twoWeeksHero: {
+    src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Lan%20Ha%20Bay-Cat%20Ba%20Vietnam-Andres%20Larin.jpg?width=1800',
+    alt: 'Kalkstensøer i Lan Ha Bay i Nordvietnam',
+    caption: 'På to uger ville vi prioritere fem stærke stop frem for at flytte hotel hver anden dag.',
+    ratio: '16 / 7',
+  },
+  plans: {
+    tenDays: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Hoan%20Kiem%20Lake%20in%20Hanoi%2C%20Vietnam.png?width=1200',
+      alt: 'Hoan Kiem Lake i Hanoi',
+    },
+    fourteenDays: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/An%20Bang%20Beach%20%2846353448872%29.jpg?width=1200',
+      alt: 'An Bang Beach ved Hoi An',
+    },
+    threeWeeks: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Phu%20Quoc%20sunset.jpg?width=1200',
+      alt: 'Solnedgang på Phu Quoc',
+    },
+  },
+  tours: {
+    halong: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Ha%20Long%20Bay%20Vietnam%20%2848731261248%29.jpg?width=1200',
+      alt: 'Cruise og kalkstensøer i Ha Long Bay',
+    },
+    ninhBinh: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Tourist%20boats%20in%20Tam%20Coc%2C%20Vietnam.jpg?width=1200',
+      alt: 'Både ved Tam Coc i Ninh Binh',
+    },
+    streetFood: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Hanoi%20street%20food.jpg?width=1200',
+      alt: 'Street food i Hanoi',
+    },
+  },
+  hotels: {
+    hanoi: {
+      src: 'https://images.unsplash.com/photo-1555921015-5532091f6026?auto=format&fit=crop&w=1200&q=82',
+      alt: 'Hanoi',
+    },
+    hoiAn: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/An%20Bang%20Beach%20%2846353448872%29.jpg?width=1200',
+      alt: 'Hoi An og An Bang Beach',
+    },
+    hcmc: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Ho%20Chi%20Minh%20City%20skyline%20%2849399217481%29.jpg?width=1200',
+      alt: 'Ho Chi Minh City skyline',
+    },
+    phuQuoc: {
+      src: 'https://commons.wikimedia.org/wiki/Special:FilePath/Phu%20quoc%20beach.JPG?width=1200',
+      alt: 'Strand på Phu Quoc',
+    },
+  },
+};
